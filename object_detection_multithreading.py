b0dfbf3346fd0c87e084e04594c1728ab7a9bb53
ibeckermayer <ibeckermayer@gmail.com>
2018-03-10 23:11:54 -0800

    changed Queue size to 1 which appears to give the best output and has no real effect on fps

--- object_detection_multithreading.py
+++ object_detection_multithreading.py
@@ -91,7 +91,7 @@ if __name__ == '__main__':
                         default=360, help='Height of the frames in the video stream.')
     args = parser.parse_args()
 
-    input_q = Queue(100)  # fps is better if queue is higher but then more lags
+    input_q = Queue(1)  # fps is better if queue is higher but then more lags
     output_q = Queue()
     for i in range(1):
         t = Thread(target=worker, args=(input_q, output_q))
